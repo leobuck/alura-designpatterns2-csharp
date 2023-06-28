efb27d0297816580c712974d6ca5a89d61fe960c
leobuck <leobuck12@gmail.com>
2023-06-27 21:40:41 -0300

    Fábricas e o problema de criação de objetos

---
+++ DesignPatterns2/Capitulo01/ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Capitulo01
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DesignPatterns;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
---
+++ DesignPatterns2/Program.cs
@@ -0,0 +1,6 @@
+﻿using DesignPatterns2.Capitulo01;
+using System.Data;
+
+IDbConnection conexao = new ConnectionFactory().GetConnection();
+IDbCommand comando =  conexao.CreateCommand();
+comando.CommandText = "SELECT * FROM tabela";
