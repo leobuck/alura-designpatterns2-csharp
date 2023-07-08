88811992d9dab633d73a91f9f87c9d82a060c59e
leobuck <leobuck12@gmail.com>
2023-07-08 09:13:51 -0300

    Implementando um Singleton

---
+++ DesignPatterns2/Capitulo09/ServicoSingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Capitulo09;
+
+public class ServicoSingleton
+{
+    private static Servico servico = new Servico();
+
+    public Servico Instancia
+    {
+        get
+        {
+            return servico;
+        }
+    }
+}
